26f39111053754f04209e8d540d97c3bff626e19
Daniel Truong <damnitruong@gmail.com>
2019-07-12 13:01:10 +0700

    Creating a gameplay scene with a Scene Setup prefab.

---
+++ Assets/Scripts/SceneSetup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SceneSetup : MonoBehaviour
+{
+    static SceneSetup instance = null;
+    public static SceneSetup Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake() 
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+}

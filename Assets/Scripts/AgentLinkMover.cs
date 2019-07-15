3f669f17752155a5519c3a84e7e6c5a9dd647f75
Daniel Truong <damnitruong@gmail.com>
2019-07-15 16:29:28 +0700

    NavMeshAgent now properly moves across NavMeshLinks, with 3 different styles, walking, jumping, or custom curve.

---
+++ Assets/Scripts/AgentLinkMover.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentLinkMover : MonoBehaviour
+{
+    public enum OffMeshLinkMoveMethod
+    {
+        Teleport,
+        NormalSpeed,
+        Parabola,
+        Curve
+    }
+
+    public OffMeshLinkMoveMethod myMoveMethod = OffMeshLinkMoveMethod.Parabola;
+    public AnimationCurve myAnimationCurve = new AnimationCurve();
+
+    IEnumerator Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.autoTraverseOffMeshLink = false;
+        while (true)
+        {
+            if (agent.isOnOffMeshLink)
+            {
+                if (Mathf.Abs(agent.currentOffMeshLinkData.endPos.y - transform.position.y) > 1f)
+                {
+                    yield return StartCoroutine(Parabola(agent, 4.0f, 0.5f));
+                }
+                else
+                {
+                    switch (myMoveMethod)
+                    {
+                        case OffMeshLinkMoveMethod.NormalSpeed:
+                            yield return StartCoroutine(NormalSpeed(agent));
+                            break;
+                        case OffMeshLinkMoveMethod.Parabola:
+                            yield return StartCoroutine(Parabola(agent, 4.0f, 0.5f));
+                            break;
+                        case OffMeshLinkMoveMethod.Curve:
+                            yield return StartCoroutine(Curve(agent, 0.5f));
+                            break;
+                    }
+                }
+
+                agent.CompleteOffMeshLink();
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator NormalSpeed(NavMeshAgent agent)
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        while (agent.transform.position != endPos)
+        {
+            agent.transform.position = Vector3.MoveTowards(agent.transform.position, endPos, agent.speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    IEnumerator Parabola(NavMeshAgent agent, float height, float duration)
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        float normalizedTime = 0.0f;
+        while (normalizedTime < 1.0f)
+        {
+            float yOffset = height * 4.0f * (normalizedTime - normalizedTime * normalizedTime);
+            agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
+            normalizedTime += Time.deltaTime / duration;
+            yield return null;
+        }
+    }
+
+    IEnumerator Curve(NavMeshAgent agent, float duration)
+    {
+        OffMeshLinkData data = agent.currentOffMeshLinkData;
+        Vector3 startPos = agent.transform.position;
+        Vector3 endPos = data.endPos + Vector3.up * agent.baseOffset;
+        float normalizedTime = 0.0f;
+        while (normalizedTime < 1.0f)
+        {
+            float yOffset = myAnimationCurve.Evaluate(normalizedTime);
+            agent.transform.position = Vector3.Lerp(startPos, endPos, normalizedTime) + yOffset * Vector3.up;
+            normalizedTime += Time.deltaTime / duration;
+            yield return null;
+        }
+    }
+}

c09d5af8ef5be3017b29e1d5be1d6aa0b803fabd
Daniel Truong <damnitruong@gmail.com>
2019-07-16 13:53:58 +0700

    Adding in a Time Manager to slow down time when a squad is selected.

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    const float animationSpeed = 4f;
+
+    private void OnEnable()
+    {
+        Squad.OnSquadSelected += OnSquadSelected;
+        Squad.OnSquadDeselected += OnSquadDeselected;
+    }
+
+    private void OnDisable()
+    {
+        Squad.OnSquadSelected -= OnSquadSelected;
+        Squad.OnSquadDeselected -= OnSquadDeselected;
+    }
+
+    void OnSquadSelected(Squad squad)
+    {
+        SlowTime();
+    }
+
+    void OnSquadDeselected(Squad squad)
+    {
+        NormalTime();
+    }
+
+    void SlowTime()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TransitionTimeAnimation(0.05f));
+    }
+
+    void NormalTime()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TransitionTimeAnimation(1f));
+    }
+
+    IEnumerator TransitionTimeAnimation(float timeScaleDestination)
+    {
+        float timer = 0f;
+        while (timer < 1f)
+        {
+            timer += Time.deltaTime * animationSpeed;
+            Time.timeScale = Mathf.Lerp(Time.timeScale, timeScaleDestination, timer);
+            yield return null;
+        }
+    }
+
+}

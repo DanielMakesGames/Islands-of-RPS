6f9d1d757df2a8c581b5998c1e46594d4fc8f336
Daniel Truong <damnitruong@gmail.com>
2019-07-18 23:38:43 +0700

    This class should be abstract.

--- Assets/Scripts/Units/FilteredUnitBehaviour.cs
+++ Assets/Scripts/Units/FilteredUnitBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FilteredUnitBehaviour : UnitBehaviour
+public abstract class FilteredUnitBehaviour : UnitBehaviour
 {
     public ContextFilter filter;
 }

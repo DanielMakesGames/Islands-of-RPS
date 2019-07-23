69ab9dd480a76c795ed9035c45742ed984a5959d
Daniel Truong <damnitruong@gmail.com>
2019-07-24 01:16:32 +0700

    Adding in Town Center Fire effect.

---
+++ Assets/Scripts/TownCenterFire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownCenterFire : MonoBehaviour
+{
+    TownCenter myTownCenter;
+    ParticleSystem myParticleSystem;
+
+    bool isPlaying = false;
+
+    void Awake()
+    {
+        myTownCenter = GetComponentInParent<TownCenter>();
+        myParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+
+        if (!isPlaying && myTownCenter.Heatlh < 50f)
+        {
+            isPlaying = true;
+            PlayFire();
+        }
+    }
+
+    void PlayFire()
+    {
+        myParticleSystem.Play();
+    }
+}

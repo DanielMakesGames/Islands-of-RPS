73f71ff5bfbb6f26eafc2cdc52b7b2a934e129f6
Daniel Truong <damnitruong@gmail.com>
2019-07-24 01:43:35 +0700

    Adding a smoke trail effect to enemy transports.

---
+++ Assets/Scripts/Units/EnemyTransportSmoke.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTransportSmoke : MonoBehaviour
+{
+    EnemyTransport myEnemyTransport;
+    ParticleSystem myParticleSystem;
+
+    private void Awake()
+    {
+        myEnemyTransport = GetComponentInParent<EnemyTransport>();
+        myParticleSystem = GetComponent<ParticleSystem>();
+
+        myEnemyTransport.OnEnemyTransportLanded += OnEnemyTransportLanded;
+    }
+
+    void OnEnemyTransportLanded(Node landingNode)
+    {
+        myParticleSystem.Stop();
+
+        myEnemyTransport.OnEnemyTransportLanded -= OnEnemyTransportLanded;
+    }
+}

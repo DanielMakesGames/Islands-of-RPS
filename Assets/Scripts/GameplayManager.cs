d4956a2d5d400510a50c362bb02ea210e1ab1de1
Daniel Truong <damnitruong@gmail.com>
2019-07-25 14:58:39 +0700

    Adding the Islands to the title screen UI. Can now select different islands.

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -9,23 +9,39 @@ public class GameplayManager : MonoBehaviour
 
     [SerializeField] GameObject[] Islands = null;
 
+    IslandItemSelect currentIslandItemSelect = null;
     GameObject currentIsland = null;
 
     private void OnEnable()
     {
         PlayButton.OnPressed += PlayButtonOnPressed;
         ReturnToTitleButton.OnPressed += ReturnToTitleButtonOnPressed;
+        IslandItemSelect.OnIslandSelected += OnIslandSelected;
     }
 
     private void OnDisable()
     {
         PlayButton.OnPressed -= PlayButtonOnPressed;
         ReturnToTitleButton.OnPressed -= ReturnToTitleButtonOnPressed;
+        IslandItemSelect.OnIslandSelected -= OnIslandSelected;
+    }
+
+    void OnIslandSelected(IslandItemSelect island)
+    {
+        currentIslandItemSelect = island;
     }
 
     void PlayButtonOnPressed()
     {
-        currentIsland = Instantiate(Islands[0]);
+        for (int i = 0; i < Islands.Length; ++i)
+        {
+            if (Islands[i].name == currentIslandItemSelect.IslandName)
+            {
+                currentIsland = Instantiate(Islands[i]);
+                break;
+            }
+        }
+
         currentIsland.transform.parent = transform;
         currentIsland.transform.localPosition = Vector3.zero;
         currentIsland.transform.localRotation = Quaternion.identity;

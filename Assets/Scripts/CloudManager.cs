23c17903e3a0982832a5100db90fc3bd0b6bb37e
Daniel Truong <damnitruong@gmail.com>
2019-07-30 02:28:27 +0700

    Adding in a cloud manager to disable clouds when we're back in the Title Screen.

---
+++ Assets/Scripts/CloudManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+    ParticleSystem[] myParticleSystems;
+
+    private void Awake()
+    {
+        myParticleSystems = GetComponentsInChildren<ParticleSystem>();
+
+        StopAllParticleSystems();
+    }
+
+    private void OnEnable()
+    {
+        PlayButton.OnPressed += PlayAllParticleSystems;
+        ReturnToTitleButton.OnPressed += StopAllParticleSystems;
+    }
+
+    private void OnDisable()
+    {
+        PlayButton.OnPressed -= PlayAllParticleSystems;
+        ReturnToTitleButton.OnPressed -= StopAllParticleSystems;
+    }
+
+    void PlayAllParticleSystems()
+    {
+        for (int i = 0; i < myParticleSystems.Length; ++i)
+        {
+            myParticleSystems[i].Play();
+        }
+    }
+
+    void StopAllParticleSystems()
+    {
+        for (int i = 0; i < myParticleSystems.Length; ++i)
+        {
+            myParticleSystems[i].Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+    }
+}
